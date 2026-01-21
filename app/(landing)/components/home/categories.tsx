83153734f4aa61b2b2149fcf6c88456a000837a0
inaayy <nurussakinah2205@gmail.com>
2026-01-21 09:31:56 +0700

    update

--- app/(landing)/components/home/categories.tsx
+++ app/(landing)/components/home/categories.tsx
@@ -1,35 +1,41 @@
 import Link from "next/dist/client/link";
 import { FiArrowRight } from "react-icons/fi";
 import Image from "next/image";
+import { Category } from "@/app/types";
+import { getImageUrl } from "@/app/lib/api";
 
 const categoryList = [
     {
         name: "running",
         imgUrl: "category-running.svg",
-    },
-    {
-        name: "tennis",
-        imgUrl: "category-tennis.svg",
-    },
-    {
-        name: "basketball",
-        imgUrl: "category-basketball.svg",
-    },
-    {
-        name: "football",
-        imgUrl: "category-football.svg",
-    },
-    {
-        name: "badminton",
-        imgUrl: "category-badminton.svg",
-    },
-    {
-        name: "swimming",
-        imgUrl: "category-swimming.svg",
-    }
-]
+    }]
+//     {
+//         name: "tennis",
+//         imgUrl: "category-tennis.svg",
+//     },
+//     {
+//         name: "basketball",
+//         imgUrl: "category-basketball.svg",
+//     },
+//     {
+//         name: "football",
+//         imgUrl: "category-football.svg",
+//     },
+//     {
+//         name: "badminton",
+//         imgUrl: "category-badminton.svg",
+//     },
+//     {
+//         name: "swimming",
+//         imgUrl: "category-swimming.svg",
+//     }
+// ]
 
-const CategoriesSection = () => {
+type TCategoriesProps = {
+    categories: Category[];
+}
+
+const CategoriesSection = ({ categories }: TCategoriesProps) => {
     return <section id="category-section" className="container mx-auto pb-20">
         <div className="flex justify-between">
             <h2 className="font-bold text-2xl">Browse By Categories</h2>
@@ -40,12 +46,12 @@ const CategoriesSection = () => {
         </div>
 
         <div className="grid grid-cols-6 gap-12 mt-8">
-            {categoryList.map((category, index) => (
+            {categories.map((category) => (
                 <div className="rounded-lg bg-gradient-to-r from-[#F1F1F1] to-[#F7F7F7] w-full aspect-square flex justify-center" 
-                key={index}>
+                key={category._id}>
                     <div className="self-center">
                         <Image 
-                        src={`/images/categories/${category.imgUrl}`} 
+                        src={getImageUrl(category.imageUrl)} 
                         width={86} 
                         height="86" 
                         alt={category.name}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
+import categories from "./(landing)/components/home/categories";
 import Categories from "./(landing)/components/home/categories";
 import HeroSection from "./(landing)/components/home/hero";
 import ProductsSection from "./(landing)/components/home/products";
+import { getAllCategories } from "./services/category.service";
 
-export default function Home() {
+export default async function Home() {
+  const categories = await getAllCategories();
+  
   return (
     <main>
       <HeroSection/>
-      <Categories/>
+      <Categories categories={categories}/>
       <ProductsSection/>
     </main>
   );
