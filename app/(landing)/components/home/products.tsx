14937a293d083779ed1de062c5edd593dd7a1454
inaayy <nurussakinah2205@gmail.com>
2026-01-29 15:23:19 +0700

    update

--- app/(landing)/components/home/products.tsx
+++ app/(landing)/components/home/products.tsx
@@ -46,7 +46,7 @@ const ProductsSection = ({products}: TProductsProps) => {
             </div>
             <h3 className="font-medium text-lg mb-1.5 mt-4">{product.name}</h3>
             <div className="flex justify-between mb-8">
-              <div className="text-gray-500">{product.category.name}</div>
+              <div className="text-gray-500">{product.category?.name}</div>
               <div className="front-medium text-primary">
                 {Intl.NumberFormat("id-ID", {
                   style: "currency",
